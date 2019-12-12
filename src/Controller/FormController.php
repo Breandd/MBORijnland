7da198392be8a15077fcb2984e75de41d6f70042
Breandd <roy.vissers@hotmail.com>
2019-12-12 11:25:02 +0100

    Added the correct buttons in the form, fixed progress bar

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -5,26 +5,31 @@ namespace App\Controller;
 use App\Entity\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\QuestionRepository;
 
 class FormController extends AbstractController
 {
     /**
      * @Route("/form/{id}", name="form")
      */
-    public function index($id){
-        if (isset($_POST['volgende'])){
-            $id = $id + 1;
-        }
-        if (isset($_POST['vorige'])){
-            $id = $id - 1;
+    public function index($id, Request $request, QuestionRepository $questionRepository){
+        if ($id < 1) {
+            return $this->redirectToRoute('form', ['id' => 1]);
         }
 
-        $questions = $this->getDoctrine()->getRepository(Question::class)->find(['id' => $id]);
+        $questions = $questionRepository->findAll();
+        $question = $questionRepository->find($id);
+                
+        if ($question === null) {
+            // Go to endpage
+        }
 
         return $this->render('survey/fillsurvey.html.twig', [
             'controller_name' => 'FormController',
             'id' => $id,
-            'question' => $questions,
+            'length' => count($questions),
+            'question' => $questions
         ]);
     }
 }
